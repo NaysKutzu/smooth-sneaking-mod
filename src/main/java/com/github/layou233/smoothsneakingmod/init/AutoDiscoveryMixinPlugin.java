6e383b2164b3abdc1df6a209593698888eff392e
GitHub Action <action@github.com>
2024-07-25 11:45:39 +0000

    Initialize template repository proper
    Set package name to com.github.layou233.smoothsneakingmod
    Set project name to smooth-sneaking-mod
    Set modid to smoothsneakingmod

--- src/main/java/com/github/layou233/smoothsneakingmod/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/layou233/smoothsneakingmod/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.layou233.smoothsneakingmod.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
